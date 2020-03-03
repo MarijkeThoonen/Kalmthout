98f35a70151f77503a5e6c4031b4f9a356ae8f9e
THOONEN <marijke.thoonen@inbo.be>
2020-03-03 13:45:52 +0100

    data opkuisen voor analyse

--- Scripts/Prep_data_VEGCOM_KH.R
+++ Scripts/Prep_data_VEGCOM_KH.R
@@ -28,55 +28,55 @@ library(tidyr)
 library(plyr)
 
 #Import and control of dataset
-VEGCOM.KH <- read.csv2(
+vegcom_kh <- read.csv2(
   "Data/Basisdatasets/Veg_2011_2012_2013_CLEAN_27nov2018.csv",
   stringsAsFactors = FALSE)
 
 #Verkennen datatype en frame
-str(VEGCOM.KH)
-dim(VEGCOM.KH)
+str(vegcom_kh)
+dim(vegcom_kh)
 
 #Deleten van de IDnummer
-VEGCOM.KH$ID <- NULL
+vegcom_kh$ID <- NULL
 
 #berekenen van % hemicellulose
-VEGCOM.KH$HEMCEL_pct <- (VEGCOM.KH$NDF - VEGCOM.KH$ADF)
+vegcom_kh$HEMCEL_pct <- (vegcom_kh$NDF - vegcom_kh$ADF)
 
 #berekenen van % cellulose
-VEGCOM.KH$CEL_pct <- (VEGCOM.KH$ADF - VEGCOM.KH$ADL)
+vegcom_kh$CEL_pct <- (vegcom_kh$ADF - vegcom_kh$ADL)
 
 #kolomnaam veranderen kolomnaam ADL naar LIG_pct
-VEGCOM.KH <- rename(VEGCOM.KH,c("ADL" = "LIG_pct"))
+vegcom_kh <- rename(vegcom_kh,c("ADL" = "LIG_pct"))
 
 #kolomnaam RE veranderen in RE_NIR
-VEGCOM.KH <- rename(VEGCOM.KH, c("RE" = "RE_NIR_pct"))
+vegcom_kh <- rename(vegcom_kh, c("RE" = "RE_NIR_pct"))
 
 #RE_DS naam veranderen in RE_NATCHEM_DS_pct
-VEGCOM.KH <- rename(VEGCOM.KH, c("RE_DS" = "RE_NATCHEM_DS_pct"))
+vegcom_kh <- rename(vegcom_kh, c("RE_DS" = "RE_NATCHEM_DS_pct"))
 
 #DS_105, VOCHT_105, N_DS omrekenen vanuit g/kg naar %
-VEGCOM.KH$DS_pct <- (VEGCOM.KH$DS_105*0.1)
-VEGCOM.KH$VOCHT_pct <- (VEGCOM.KH$VOCHT_105*0.1)
-VEGCOM.KH$N_DS_pct <- (VEGCOM.KH$N_DS*0.1)
+vegcom_kh$DS_pct <- (vegcom_kh$DS_105*0.1)
+vegcom_kh$VOCHT_pct <- (vegcom_kh$VOCHT_105*0.1)
+vegcom_kh$N_DS_pct <- (vegcom_kh$N_DS*0.1)
 
 #RE uitdrukken op droge stofgehalte voor vergelijking met RE_NATCHEM
-VEGCOM.KH$RE_NIR_DS_pct <- (VEGCOM.KH$RE_NIR*(1000/VEGCOM.KH$DS_105))
+vegcom_kh$RE_NIR_DS_pct <- (vegcom_kh$RE_NIR*(1000/vegcom_kh$DS_105))
 
 #maanden uitdrukken in cijfers
-VEGCOM.KH$M <- revalue(VEGCOM.KH$M, c("jun" = "6", "jul" = "7", "aug" = "8",
+vegcom_kh$M <- revalue(vegcom_kh$M, c("jun" = "6", "jul" = "7", "aug" = "8",
                                       "sep" = "9"))
 #of
 
-#VEGCOM.KH$M[VEGCOM.KH$M == "jun"] <- "6"
-#VEGCOM.KH$M[VEGCOM.KH$M == "jul"] <- "7"
-#VEGCOM.KH$M[VEGCOM.KH$M == "aug"] <- "8"
-#VEGCOM.KH$M[VEGCOM.KH$M == "sep"] <- "9"
+#vegcom_kh$M[vegcom_kh$M == "jun"] <- "6"
+#vegcom_kh$M[vegcom_kh$M == "jul"] <- "7"
+#vegcom_kh$M[vegcom_kh$M == "aug"] <- "8"
+#vegcom_kh$M[vegcom_kh$M == "sep"] <- "9"
 
 #nieuwe kolom maken voor Brand in 2011
-VEGCOM.KH <- VEGCOM.KH %>% 
-  mutate(Brand2011 = ifelse(EXC_ID %in% c("WD1","WD2", "LV1", "LV2"), "Ja", "Nee"))
+vegcom_kh <- vegcom_kh %>% 
+  mutate(Brand2011 = ifelse(EXC_ID %in% c("WD1","WD2", "LV1", "LV2"), "ja", "nee"))
          
-VEGCOM.KH <- VEGCOM.KH %>% 
+vegcom_kh <- vegcom_kh %>% 
   select(EXC_ID, BEH_TYP, M, Y, VEG_TYP, DS_pct, VOCHT_pct, 
                       LIG_pct, CEL_pct, HEMCEL_pct, N_DS_pct, RE_NIR_DS_pct, 
                       RE_NATCHEM_DS_pct, Brand2011)
@@ -85,11 +85,11 @@ VEGCOM.KH <- VEGCOM.KH %>%
 install.packages("janitor")
 library(janitor)
 
-vegcom_kh <- VEGCOM.KH %>%
+vegcom_kh <- vegcom_kh %>%
   clean_names()
 
 # Wegschrijven van de dataset
-write.csv2(VEGCOM.KH, row.names = FALSE,
+write.csv2(vegcom_kh, row.names = FALSE,
            file = "Data/Afgeleide datasets/vegcom_kh.csv")
 
 
@@ -98,26 +98,26 @@ write.csv2(VEGCOM.KH, row.names = FALSE,
 ##library(stringr)
 
 #nieuwe variabele maken met de jaarlocatie
-##VEGCOM.KH$yearlocation <- paste(VEGCOM.KH$EXC_ID, VEGCOM.KH$Y, sep = " ")
+##vegcom_kh$yearlocation <- paste(vegcom_kh$EXC_ID, vegcom_kh$Y, sep = " ")
 
 #nieuwe variabele maken met de maandlocatie
-##VEGCOM.KH$monthlocation <- paste(VEGCOM.KH$EXC_ID, VEGCOM.KH$M, sep = " ")
+##vegcom_kh$monthlocation <- paste(vegcom_kh$EXC_ID, vegcom_kh$M, sep = " ")
 
 #nieuwe variabele maken met de maand en jaar
-##VEGCOM.KH$obs_M_Y <- paste(VEGCOM.KH$M, VEGCOM.KH$Y, sep = " ")
+##vegcom_kh$obs_M_Y <- paste(vegcom_kh$M, vegcom_kh$Y, sep = " ")
 
 #nieuwe variabele maken met de maand, datum en locatie
-##VEGCOM.KH$datelocation <- paste(VEGCOM.KH$EXC_ID, VEGCOM.KH$obs_M_Y, sep = " ")
+##vegcom_kh$datelocation <- paste(vegcom_kh$EXC_ID, vegcom_kh$obs_M_Y, sep = " ")
 
 #nieuwe variabele maken met beheertype per locatie
-##VEGCOM.KH$EXC_ID_BEH_TYP <- paste(VEGCOM.KH$EXC_ID, VEGCOM.KH$BEH_TYP, sep = " ")
+##vegcom_kh$EXC_ID_BEH_TYP <- paste(vegcom_kh$EXC_ID, vegcom_kh$BEH_TYP, sep = " ")
 
 #nieuwe variabele maken met vegetatietype per locatie
-##VEGCOM.KH$EXC_ID_VEG_TYP <- paste(VEGCOM.KH$EXC_ID, VEGCOM.KH$VEG_TYP, sep = " ")
+##vegcom_kh$EXC_ID_VEG_TYP <- paste(vegcom_kh$EXC_ID, vegcom_kh$VEG_TYP, sep = " ")
 
 #nieuwe variabele maken met unieke sleutel per staal EXC_ID BEH_TYP VEG_TYP M Y 
-#VEGCOM.KH$SAMPLE_ID <- paste(VEGCOM.KH$EXC_ID, VEGCOM.KH$BEH_TYP,
-                             #VEGCOM.KH$VEG_TYP, VEGCOM.KH$M, VEGCOM.KH$Y, 
+#vegcom_kh$SAMPLE_ID <- paste(vegcom_kh$EXC_ID, vegcom_kh$BEH_TYP,
+                             #vegcom_kh$VEG_TYP, vegcom_kh$M, vegcom_kh$Y, 
                              #sep = " ")
 
 
--- Scripts/prep_data_vegstr_kh.R
+++ Scripts/prep_data_vegstr_kh.R
@@ -1,6 +1,46 @@
-# aanmaken van een .csv met de data van de vegetatiestructuur
+library(tidyverse)
+library(plyr)
 
-#nieuwe kolom maken voor type begrazing
-summarise(VEGCOM.KH, unique(EXC_ID))
+#Import and control of dataset
+vegstr_kh <- read.csv2("./Data/Basisdatasets/HTbl_Exclosure_opname_2011_2014.csv",
+  stringsAsFactors = FALSE)
 
-mutate(BEG_TYP = ifelse(EXC_ID %in% c("WD1","WD2", "LV1", "LV2"), "extensief", "gehoed" ))
+#Verkennen datatype en frame
+str(vegstr_kh)
+dim(vegstr_kh)
+
+#veranderen ID_Excosure in afgekorte namen
+vegstr_kh$ID_Exclosure <- mapvalues(vegstr_kh$ID_Exclosure, from = c(1, 2, 3, 4, 
+                                                                     5, 6, 7, 8, 
+                                                                     9),
+                                    to = c("GB1", "GB2", "WD1", "WD2","LV1", 
+                                           "LV2", "VEN1", "VEN2", "HD"))
+
+#Deleten van de ID_Exclosure_opname, Deelgebied, Opmerking, ...
+vegstr_kh <- vegstr_kh %>% 
+  select(-ID_Exclosure_opname, -Deelgebied, -Opmerking, - Afstand_m, 
+         -Hoek_gr, -Bedekking.mos, -Bedekking.kale.bodem, -strooisel.ingezameld,
+         -bodemstalen.ingezameld, -H1_cm, -H2_cm, -H3_cm, -H4_cm, 
+         -H5_cm, -H6_cm, -H7_cm, -H8_cm)
+
+#kolomnamen hernoemen, let op er zijn twee functies rename in tidyverse en dplyr,
+# daarom specifiek deze van dplyr oproepen door dplyr::rename, anders error!
+vegstr_kh <- dplyr::rename(vegstr_kh, exc_id = ID_Exclosure)
+vegstr_kh <- dplyr::rename(vegstr_kh, beh_typ = Begraasd.Niet.Begr, 
+                           brand2011 = Afgebrand.2011,
+                           y = jaar, m = maand, zone = ID_Exclosure_zone)
+vegstr_kh <- dplyr::rename(vegstr_kh, veg_aanw = Vegetatie.aanwezig)
+
+#waarden kol beh_typ hernoemen
+distinct(vegstr_kh, beh_typ)
+vegstr_kh$beh_typ <- mapvalues(vegstr_kh$beh_typ, from = c("NB", "B"),
+                                    to = c("EXC", "BGR"))
+
+#waarden kol veg_aanw en kol brand2011 hernoemen
+distinct(vegstr_kh, veg_aanw)
+vegstr_kh$veg_aanw <- mapvalues(vegstr_kh$veg_aanw, from = c("WAAR", "ONWAAR"),
+                               to = c("ja", "nee"))
+
+distinct(vegstr_kh, veg_aanw)
+vegstr_kh$brand2011 <- mapvalues(vegstr_kh$brand2011, from = c("WAAR", "ONWAAR"),
+                                to = c("ja", "nee"))
